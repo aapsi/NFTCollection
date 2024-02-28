61b9c0df95deebc06dd9cfb8ede78d210ea19247
aapsi <aapsi0001@gmail.com>
2024-02-29 00:22:43 +0530

    Basic NFT contract

--- src/basicNFT.sol
+++ src/basicNFT.sol
@@ -1,3 +1,34 @@
-// SPDX-License-Identifier: mit
-pragma solidity ^0.8.24;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract BasicNft is ERC721 {
+    error BasicNft__TokenUriNotFound();
+
+    uint256 public tokenCounter;
+    mapping (uint256 tokenId => string tokenUri) tokenIdToUri;
+
+    constructor() ERC721("Dogie", "DOG") {
+        tokenCounter = 0;
+    }
+
+    function mint(string memory tokenURI) public {
+        tokenIdToUri[tokenCounter] = tokenURI;
+        _safeMint(msg.sender, tokenCounter);
+        tokenCounter++;
+    }
+
+    function getTokenUri(uint256 Id) public view returns(string memory){
+        if(ownerOf(Id) == address(0)) {
+            revert BasicNft__TokenUriNotFound();
+        }
+        return tokenIdToUri[Id];
+    }
+
+    function getCurrentTokenId() public view returns(uint256) {
+        return tokenCounter;
+    }
+
+
+}
